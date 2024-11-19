0b6e7e3ffb0804d08d632329a74762b7874a2f81
Paulo Campos Bezerra <1259926@sga.pucminas.br>
2024-11-19 20:10:51 -0300

    Update obi1.c

--- obi1.c
+++ obi1.c
@@ -1,35 +1,35 @@
+// Torneio de tênis, questão da obi (Olimpíada Brasileira de Informática).
+
+
 #include <stdio.h>
+#include<stdlib.h>
 
 int main() {
-    char antigaLetra, novaLetra, palavra[100];
+    char resultado[6];
+    int vitorias = 0;
 
 
+    printf("Digite qual o resutaldo da partida: \n");
+   for(int i = 0; i < 6; i++){
+        scanf("%c", &resultado[i]);
+            getchar();
+    if (resultado [i]== 'v' || resultado[i] == 'V'){
+        vitorias++;
+    }
+   }
 
-    printf("Digite a letra a ser substitu�da: ");
-    scanf(" %c", &antigaLetra);
-    getchar();
+   if (vitorias >= 5){
+    printf("\n 1 \n");
+   }
+    if (vitorias >= 3 && vitorias < 5){
+    printf("\n 2 \n");
+   }
+    if (vitorias >= 1 && vitorias < 3){
+    printf("\n 3 \n");
+   }
+   if(vitorias == 0){
+   printf("\n lamento mas vc nn podera continuar treinando \n");
+   }
 
-
-    printf("Digite a nova letra: ");
-    scanf(" %c", &novaLetra);
-    getchar();
-
-
-    printf("Digite a palavra: ");
-    scanf("%s", palavra);
-    getchar();
-
-    for(int i = 0; i < 100; i++){
-        if (palavra[i] == antigaLetra){
-            palavra[i] = novaLetra;
-
-           }
-        }
-
-
-
-    printf("Palavra alterada: %s\n", palavra);
-
-    return 0;
 }
 
